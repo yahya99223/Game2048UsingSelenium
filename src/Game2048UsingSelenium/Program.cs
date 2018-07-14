f80bad2b874b804b5f62b5782484f14d2a7b5813
yahya <yahya.haj.hus@gmail.com>
2018-07-14 20:59:32 +0300

    fixed reference issue

--- src/Game2048UsingSelenium/Program.cs
+++ src/Game2048UsingSelenium/Program.cs
@@ -3,13 +3,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Game2048UsingSelenium
 {
